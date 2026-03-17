6f26ca8e3ae3a875a1c3f119464abe5dca539446
cerenozben072 <ceren.ozben@ozu.edu.tr>
2026-03-17 10:38:06 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,51 +1,66 @@
 // I got help from chatgpt for randomness
+
+const gfxs = [];
 let catColor;
 
+const markers = [60, 61];
+
 function setup() {
-   createARCanvas(600, 600,P2D, {scale: 30,markerId:60});
+  markers.forEach((id) => {
+    let gfx = createARGraphics(600, 600, P2D, { scale: 3, markerId: id });
+    gfxs.push(gfx);
+  });
 
-
+  p5SimpleAREnableGesture(true);
   catColor = randomColor();
 }
 
 function draw() {
-  background(240);
+  gfxs.forEach((gfx) => {
+    gfx.clear();
 
-  // cat
-  fill(catColor);
-  noStroke();
+    // cat
+    gfx.fill(catColor);
+    gfx.noStroke();
 
-  // head
-  ellipse(300, 320, 220, 200);
+    // head
+    gfx.ellipse(300, 320, 220, 200);
 
-  // ears
-  triangle(230, 250, 180, 170, 260, 190);
-  triangle(370, 250, 340, 190, 420, 170);
+    // ears
+    gfx.triangle(230, 250, 180, 170, 260, 190);
+    gfx.triangle(370, 250, 340, 190, 420, 170);
 
-  // eyes
-  fill(30);
-  ellipse(260, 320, 18, 18);
-  ellipse(340, 320, 18, 18);
+    // eyes
+    gfx.fill(30);
+    gfx.ellipse(260, 320, 18, 18);
+    gfx.ellipse(340, 320, 18, 18);
 
-  // nose
-  triangle(300, 345, 290, 360, 310, 360);
+    // nose
+    gfx.triangle(300, 345, 290, 360, 310, 360);
 
-  // whiskers
-  stroke(30);
-  strokeWeight(4);
+    // whiskers
+    gfx.stroke(30);
+    gfx.strokeWeight(4);
 
-  // left
-  line(240, 370, 170, 360);
-  line(240, 372, 170, 372);
-  line(240, 374, 170, 384);
+    // left
+    gfx.line(240, 370, 170, 360);
+    gfx.line(240, 372, 170, 372);
+    gfx.line(240, 374, 170, 384);
 
-  // right
-  line(360, 370, 430, 360);
-  line(360, 372, 430, 372);
-  line(360, 374, 430, 384);
+    // right
+    gfx.line(360, 370, 430, 360);
+    gfx.line(360, 372, 430, 372);
+    gfx.line(360, 374, 430, 384);
+  });
 }
-// when pressed color changes
+
+// mouse click
 function mousePressed() {
+  catColor = randomColor();
+}
+
+// touch (telefon için)
+function touchStarted() {
   catColor = randomColor();
 }
 
